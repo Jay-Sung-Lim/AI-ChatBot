bdd240d76f866197b38b80c4af742efbced902ff
Jaesung Lim <limjaes@sheridancollege.ca>
2024-01-21 04:40:27 -0500

    File ChatBot

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+"""
+# Hello!
+            
+Welcome to my hackville2024 project homepage!
+            
+Here are the projects that I made:
+            
+- [Website Chatbot](/Website_ChatBot)
+- [File Chatbot](/File_ChatBot)
+
+"""
+)
